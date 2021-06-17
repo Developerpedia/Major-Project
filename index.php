36547715413ae098f1cfa8bae03e1660d304b36d
Ritav Tripathi <68114246+Developerpedia@users.noreply.github.com>
2021-06-17 14:39:43 +0000

    yes

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+.container{
+    display:table;
+    position:absolute;
+    left:30%;
+    top:25%;
+}
+
+h1{
+  Font-family:monospace;
+  font-size:200%;
+  font-weight: 100%;
+  text-align:center;
+  padding-right:1ch;
+  
+}
+p{
+    font-family:'Times New Roman';
+    font-size:130%;
+    text-align:center;
+}
+
+.admin{
+  vertical-align: middle;
+  width: 140px;
+  height: 140px;
+  border-radius: 50%;
+  margin-left:15ch;
+}
+
+ .row a {
+  display: inline-block;
+  color: #3498db;
+  font-family:monospace;
+  font-size:130%;
+  text-align: center;
+  text-decoration: none;
+}
+
+ a:hover{
+  color:#2ecc71;
+ }
+
+ .row{
+     display:table;
+     padding-bottom:25ch;
+     margin-right:2ch;
+ }
+
+</style>
+</head>
+<body>
+<div class="container">
+    <h1>College management system</h1>
+<p>"An investment in knowledge pays the best interest"</p>
+
+<img src="admin.png" alt="Avatar" class="admin">
+
+
+<div class="row">
+<a style="margin-left:18ch" href="admin.php">Admin</a>
+</div>
+</div>
+</body>
+</html>
